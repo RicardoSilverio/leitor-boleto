e560c2d0344e56aa829582a317cdeaf336c517ab
silvr <ricardo.silverio.souza@gmail.com>
2017-07-04 20:03:01 -0300

    Implementação do boleto de concessionária

--- src/br/com/silvr/leitorboleto/model/Formatador.java
+++ src/br/com/silvr/leitorboleto/model/Formatador.java
@@ -1,11 +1,33 @@
 package br.com.silvr.leitorboleto.model;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.regex.Pattern;
 
 class Formatador {
 	
 	private static final Pattern VALOR_ZERADO = Pattern.compile("^0+$");
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public static Calendar getDataVencimento(String representacao) {
+		
+		int dia = Integer.parseInt(representacao.substring(6));
+		int mes = Integer.parseInt(representacao.substring(4, 6));
+		int ano = Integer.parseInt(representacao.substring(0, 4));
+		
+		Calendar dataVencimento = Calendar.getInstance();
+		dataVencimento.setLenient(false);
+		
+		try {
+			dataVencimento.set(ano, mes - 1, dia);
+			DATE_FORMAT.format(dataVencimento.getTime());
+			return dataVencimento;
+		} catch(Exception e) {
+			return null;
+		}
+		
+	}
 	
 	public static BigDecimal getValor(String representacao) {
 		
